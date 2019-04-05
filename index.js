047f7eb4a1fd84b933ebb8a2cc330cb428719a19
m-addie-k <kmtestsite0609@gmail.com>
2019-04-05 11:10:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 
-import React, { Component } from "react";
-
+import App from './js/App.js';
 import './styles/App.css';
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
-    }
-}
 
 export default App;
